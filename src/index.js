8ec8cea430c8a5344ffb6afeceb83d7c66bbd707
Rajat Shukla <shukla.rajat@outlook.com>
2021-05-26 18:50:24 +0200

    building form component and signin page

--- src/index.js
+++ src/index.js
@@ -5,23 +5,19 @@ import { App } from './app';
 import { FirebaseContext } from './context/firebase';
 
 const config = {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-}
-
-const firebase = window.firebase.initializeApp(config);
+  apiKey: 'AIzaSyDAU60M2FDHNsYt9uGMpRezw3FGVIX6Jrs',
+  authDomain: 'netflix-c8ae9.firebaseapp.com',
+  databaseURL: 'https://netflix-c8ae9.firebaseio.com',
+  projectId: 'netflix-c8ae9',
+  storageBucket: 'netflix-c8ae9.appspot.com',
+  messagingSenderId: '590575805931',
+  appId: '1:590575805931:web:f809092b6cd55306fa25ae',
+};
 
 ReactDOM.render(
-    <>
-        <FirebaseContext.Provider value={{ firebase: window.firebase }}>
-            <GlobalStyles />
-            <App />
-        </FirebaseContext.Provider>
-    </>, 
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+        <GlobalStyles />
+        <App />
+    </FirebaseContext.Provider>, 
     document.getElementById('root')
 );
