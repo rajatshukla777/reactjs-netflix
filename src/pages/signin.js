80194036f4ad9aad56add818433afc4b495bab79
rajat shukla <shukla.rajat@outlook.com>
2021-05-28 22:29:04 +0200

    change done

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,22 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
 import { Form } from '../components';
 import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import * as ROUTES from '../constants/routes';
 
 export default function Signin() {
-    const [error, setError] = useState('');
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+    
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
     
     const isInvalid = password === '' | emailAddress === '';
     
     const handleSignin = (event) => {
         event.preventDefault();
         
-        // call in here to firebase to authenticate the user
-        // if there's an error, populate the error state
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(emailAddress, password)
+            .then(() => {
+                setEmailAddress('');
+                setPassword('');
+                setError('');
+                history.push(ROUTES.BROWSE);
+            })
+            .catch((error) => setError(error.message));
     }
     
     return (
+        <>
         <HeaderContainer>
             <Form>
                 <Form.Title>Sign In</Form.Title>
@@ -48,5 +64,7 @@ export default function Signin() {
                 </Form.Base>
             </Form>
         </HeaderContainer>
+        <FooterContainer />
+        </>
     )
 }
